2162115fa5c019da0d8ed5e2707d45084b3c83b5
Hanns Holger Rutz <contact@sciss.de>
2016-09-05 12:37:48 +0200

    restore mono example

---
+++ src/main/scala/at/iem/sysson/experiments/ScenarioMono.scala
@@ -0,0 +1,32 @@
+package at.iem.sysson.experiments
+
+import at.iem.scalacollider.ScalaColliderDOT
+import at.iem.sysson.experiments.Ops._
+import de.sciss.file._
+import de.sciss.synth.ugen.{Dust, Out, Pan2, SinOsc, WhiteNoise}
+import de.sciss.synth.{GE, SynthDef}
+
+object ScenarioMono extends App {
+  If.monolithic = true
+
+  val sd = SynthDef("monolith") {
+    val amp : GE = "amp" .kr(0.2)
+    val freq: GE = "freq".kr
+
+    val res0: GE = If (freq > 1000) Then {
+      SinOsc.ar(freq)
+    } ElseIf (freq > 100) Then {
+      Dust.ar(freq)
+    } Else {
+      WhiteNoise.ar
+    }
+
+    Out.ar(0, Pan2.ar(res0 * amp))
+  }
+
+  val dotC        = ScalaColliderDOT.Config()
+  dotC.input      = sd.graph
+  dotC.graphName  = sd.name
+  dotC.rateColors = true
+  ScalaColliderDOT.writePDF(dotC, file("dot") / "mono.pdf")
+}
